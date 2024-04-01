03928d52ca2bb54bb51c317a57f99c5c1722dcf4
jihye9871 <jihye9871@naver.com>
2024-04-01 23:39:42 +0900

    feat: TokenDto 구현

---
+++ src/main/java/com/onmydesk/backend/user/dto/TokenDto.java
@@ -0,0 +1,12 @@
+package com.onmydesk.backend.user.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+    private String token;
+}
