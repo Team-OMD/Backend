e7d2fd8141d1e1581b94b0779a79c248b4a6255e
jihye9871 <jihye9871@naver.com>
2024-04-02 16:34:04 +0900

    feat : Exception 구현

--- src/main/java/com/onmydesk/backend/member/exception/NotFoundMemberException.java
+++ src/main/java/com/onmydesk/backend/member/exception/NotFoundMemberException.java
@@ -4,15 +4,12 @@ public class NotFoundMemberException extends RuntimeException {
     public NotFoundMemberException() {
         super();
     }
-
     public NotFoundMemberException(String message, Throwable cause) {
         super(message, cause);
     }
-
     public NotFoundMemberException(String message) {
         super(message);
     }
-
     public NotFoundMemberException(Throwable cause) {
         super(cause);
     }
