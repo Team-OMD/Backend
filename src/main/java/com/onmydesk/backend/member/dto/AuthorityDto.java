1e573c1e4fbe44437543f76981eef18200ac7a76
jihye9871 <jihye9871@naver.com>
2024-04-02 16:11:38 +0900

    feat: JWT 관련 dto 구현

---
+++ src/main/java/com/onmydesk/backend/member/dto/AuthorityDto.java
@@ -0,0 +1,12 @@
+package com.onmydesk.backend.member.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDto {
+    private String authorityName;
+}
---
+++ src/main/java/com/onmydesk/backend/member/dto/TokenDto.java
@@ -0,0 +1,12 @@
+package com.onmydesk.backend.member.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDto {
+    private String token;
+}
