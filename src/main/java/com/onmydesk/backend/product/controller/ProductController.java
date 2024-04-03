20cd80a1ff21a5d3f17963813c2bfa4c371fd6fb
suha <suhach0523@gmail.com>
2024-04-04 01:28:05 +0900

    feat : Product 컨트롤러에 상품 개별 조회 추가

--- src/main/java/com/onmydesk/backend/product/controller/ProductController.java
+++ src/main/java/com/onmydesk/backend/product/controller/ProductController.java
@@ -1,14 +1,20 @@
 package com.onmydesk.backend.product.controller;
 
+import com.onmydesk.backend.global.ApiResponse;
 import com.onmydesk.backend.product.domain.Product;
+import com.onmydesk.backend.product.dto.ProductListResponse;
+import com.onmydesk.backend.product.dto.ProductResponse;
 import com.onmydesk.backend.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +22,22 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    private final ApiResponse apiResponse;
 
     // 상품 목록 조회
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getAllProducts() {
-        return ResponseEntity.ok(productService.getList());
+    public ResponseEntity<?> getProducts() {
+        List<Product> products = productService.getList();
+        List<ProductListResponse> productListResponses = products.stream()
+                .map(ProductListResponse::new)
+                .collect(Collectors.toList());
+        return apiResponse.success("상품 목록 조회 성공", productListResponses,HttpStatus.OK);
+    }
+
+    // 상품 개별 조회
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<?> findProduct(@PathVariable Long productId) {
+        Product product = productService.getFind(productId);
+        return apiResponse.success("개별 상품 조회 성공", new ProductResponse(product), HttpStatus.OK);
     }
 }
