4aaf8ed418b48e5822592d5f944ae1232d6c5cff
Jaehoon <gkdwngo@gmail.com>
2024-04-03 23:46:08 +0900

    refact : Post 컨트롤러 메서드 수정

--- src/main/java/com/onmydesk/backend/post/controller/PostController.java
+++ src/main/java/com/onmydesk/backend/post/controller/PostController.java
@@ -2,7 +2,6 @@ package com.onmydesk.backend.post.controller;
 
 import com.onmydesk.backend.post.domain.Post;
 import com.onmydesk.backend.post.dto.PostRequest;
-import com.onmydesk.backend.post.dto.NewPostResponse;
 import com.onmydesk.backend.post.dto.PostResponse;
 import com.onmydesk.backend.post.service.PostService;
 import com.onmydesk.backend.global.ApiResponse; // ApiResponse 클래스 임포트 필요
@@ -26,32 +25,29 @@ public class PostController {
     @PostMapping("/posts")
     public ResponseEntity<?> createPost(@RequestBody PostRequest request) {
         Post post = postService.save(request);
-        return apiResponse.success("게시글 생성 성공", new NewPostResponse(post), HttpStatus.CREATED);
+        return apiResponse.success("게시글 생성 성공", HttpStatus.CREATED);
     }
 
     // 게시글 목록 조회
     @GetMapping("/posts")
     public ResponseEntity<?> getList(){
-        List<Post> posts = postService.list();
-        List<PostResponse> postResponses = posts.stream()
-                .map(PostResponse::new)
-                .collect(Collectors.toList());
+        List<PostResponse> postResponses = postService.list();
         return apiResponse.success("게시글 목록 조회 성공", postResponses, HttpStatus.OK);
     }
 
     // 게시글 상세 조회
     @GetMapping("/posts/{postId}")
     public ResponseEntity<?> getPost(@PathVariable("postId") Long postId) {
-        Post post = postService.find(postId);
-        return apiResponse.success("게시글 상세 조회 성공", new PostResponse(post), HttpStatus.OK);
+        PostResponse postResponse = postService.find(postId);
+        return apiResponse.success("게시글 상세 조회 성공", postResponse, HttpStatus.OK);
     }
 
     // 게시글 업데이트
     @PutMapping("/posts/{postId}")
     public ResponseEntity<?> updatePost(@PathVariable("postId") Long postId,
                                         @RequestBody PostRequest request) {
-        Post post = postService.update(postId, request);
-        return apiResponse.success("게시글 업데이트 성공", new PostResponse(post), HttpStatus.OK);
+        PostResponse postResponse = postService.update(postId, request);
+        return apiResponse.success("게시글 업데이트 성공", postResponse, HttpStatus.OK);
     }
 
     // 게시글 삭제
