3d125283d3e06228d97ccbfe2816a518859bfee0
Jaehoon <gkdwngo@gmail.com>
2024-04-04 13:23:23 +0900

    feat : Post 서비스에 회원 기능 추가

--- src/main/java/com/onmydesk/backend/post/service/PostService.java
+++ src/main/java/com/onmydesk/backend/post/service/PostService.java
@@ -1,5 +1,7 @@
 package com.onmydesk.backend.post.service;
 
+import com.onmydesk.backend.member.domain.Member;
+import com.onmydesk.backend.member.service.MemberService;
 import com.onmydesk.backend.post.domain.Post;
 import com.onmydesk.backend.post.dto.PostRequest;
 import com.onmydesk.backend.post.dto.PostResponse;
@@ -19,11 +21,14 @@ import java.util.stream.Collectors;
 public class PostService {
     private final PostRepository postRepository;
     private final PostMapper postMapper;
+    private final MemberService memberService;
+    private final ServiceValidator serviceValidator;
 
     // 게시글 생성
     @Transactional
     public Post save(PostRequest request) {
-        return postRepository.save(postMapper.toEntity(request));
+        Member member = memberService.getMember();
+        return postRepository.save(postMapper.toEntity(request, member));
     }
 
     // 게시글 목록 조회
@@ -36,7 +41,7 @@ public class PostService {
 
     // 게시글 단일 조회
     public PostResponse find(Long postId) {
-        Post post =  postRepository.findById(postId)
+        Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new PostNotFoundException(postId));
         return postMapper.toResponse(post);
     }
@@ -44,8 +49,9 @@ public class PostService {
     // 게시글 업데이트
     @Transactional
     public PostResponse update(Long postId, PostRequest request) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new PostNotFoundException(postId));
+        Member member = memberService.getMember();
+        Post post = serviceValidator.validatePostOwnership(postId, member);
+
         try {
             post.update(request.getTitle(), request.getContent());
         } catch (Exception e) {
@@ -54,7 +60,9 @@ public class PostService {
         return postMapper.toResponse(post);
     }
 
-    public void delete(Long postId){
-        postRepository.deleteById(postId);
+    public void delete(Long postId) {
+        Member member = memberService.getMember();
+        Post post = serviceValidator.validatePostOwnership(postId, member);
+        postRepository.delete(post);
     }
 }
