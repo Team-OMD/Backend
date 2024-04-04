19e02a657459eeef2c8a9c1bf9da28dd30b7bb4a
Jaehoon <gkdwngo@gmail.com>
2024-04-04 12:52:38 +0900

    feat : 게시글 권한 예외 추가

--- src/main/java/com/onmydesk/backend/post/exception/PostExceptionHandler.java
+++ src/main/java/com/onmydesk/backend/post/exception/PostExceptionHandler.java
@@ -13,10 +13,16 @@ public class PostExceptionHandler {
 
     private final ApiResponse apiResponse;
 
-    // 게시글을 찾을 수 없는 경우의 예외 처리
+    // 게시글을 찾을 수 없는 경우 예외 처리
     @ExceptionHandler(PostNotFoundException.class)
     public ResponseEntity<?> handlePostNotFoundException(PostNotFoundException e) {
         return apiResponse.error(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // 게시글 수정, 삭제 권한이 없는 경우 예외처리
+    @ExceptionHandler(PostOwnershipException.class)
+    public ResponseEntity<?> handlePostOwnershipException(PostOwnershipException e) {
+        return apiResponse.error(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     // 게시글 업데이트 중 발생한 예외 처리
