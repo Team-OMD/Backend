e67ed4aecce977485f309ffde9657d431ed255bf
Jaehoon <gkdwngo@gmail.com>
2024-04-06 12:46:49 +0900

    feat : 좋아요 기능 추가

--- src/main/java/com/onmydesk/backend/post/repository/PostRepository.java
+++ src/main/java/com/onmydesk/backend/post/repository/PostRepository.java
@@ -2,6 +2,23 @@ package com.onmydesk.backend.post.repository;
 
 import com.onmydesk.backend.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+    // 좋아요 개수 증가
+    @Modifying
+    @Transactional
+    @Query("update Post p set p.heartCount = p.heartCount + 1 where p = :selectedPost")
+    void addHeartCount(Post selectedPost);
+
+    // 좋아요 개수 감소
+    @Modifying
+    @Transactional
+    @Query("update Post p set p.heartCount = p.heartCount - 1 where p = :selectedPost")
+    void subHeartCount(Post selectedPost);
 }
