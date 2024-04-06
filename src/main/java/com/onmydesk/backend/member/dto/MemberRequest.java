76f625f591b7b13048cb1fdcb468fd4f3c9906c0
jihye9871 <jihye9871@naver.com>
2024-04-07 02:59:34 +0900

    feat : MemberRequest에 Swagger 명세 및 예시 데이터 추가

--- src/main/java/com/onmydesk/backend/member/dto/MemberRequest.java
+++ src/main/java/com/onmydesk/backend/member/dto/MemberRequest.java
@@ -1,5 +1,6 @@
 package com.onmydesk.backend.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -11,19 +12,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(description = "회원이 회원가입할때 필요한 데이터")
 public class MemberRequest {
 
     @Email
     @NotBlank
+    @Schema(description = "사용자 이메일", nullable = false, example = "jihye@naver.com")
     private String email;
 
     @NotBlank
+    @Schema(description = "사용자 비밀번호", nullable = false, example = "qwe123")
     private String password;
 
     @NotBlank
+    @Schema(description = "사용자 이름", nullable = false, example = "최지혜")
     private String name;
 
     @NotBlank
+    @Schema(description = "사용자 닉네임", nullable = false, example = "최졔")
     private String nickname;
 }
 
