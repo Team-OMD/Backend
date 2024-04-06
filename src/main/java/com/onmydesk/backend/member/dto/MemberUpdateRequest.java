f116dc91da4e82ebe92bd62480c20e89ee414f4e
jihye9871 <jihye9871@naver.com>
2024-04-07 02:59:57 +0900

    feat : MemberUpdateRequest에 Swagger 명세 및 예시 데이터 추가

--- src/main/java/com/onmydesk/backend/member/dto/MemberUpdateRequest.java
+++ src/main/java/com/onmydesk/backend/member/dto/MemberUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.onmydesk.backend.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,11 +11,14 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "회원이 정보를 수정할 때 필요한 데이터")
 public class MemberUpdateRequest {
     @NotBlank
+    @Schema(description = "사용자가 바꿀 이름", nullable = false, example = "최수하")
     private String name;
 
     @NotBlank
+    @Schema(description = "사용자가 바꿀 닉네임", nullable = false, example = "귤")
     private String nickname;
 
 }
