6b03d7e3a96adf4de183e9ed12f78c6f18bcc91a
suha <suhach0523@gmail.com>
2024-04-10 17:56:31 +0900

    refact : Post 요청 리팩토링

--- src/main/java/com/onmydesk/backend/post/dto/PostRequest.java
+++ src/main/java/com/onmydesk/backend/post/dto/PostRequest.java
@@ -1,15 +1,27 @@
 package com.onmydesk.backend.post.dto;
 
-import com.onmydesk.backend.post.domain.Post;
+import com.onmydesk.backend.product.dto.ProductRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Schema(description = "게시글 생성할 때 필요한 데이터")
 public class PostRequest {
+
+    @NotBlank
+    @Schema(description = "게시글 제목", nullable = false, example = "제목")
     private String title;
+
+    @NotBlank
+    @Schema(description = "게시글 내용", nullable = false, example = "내용")
     private String content;
 
+    private List<ProductRequest> products;
 }
