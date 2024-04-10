abbf5b4894034cae7f1d3aad9f182beeb1fa02c7
suha <suhach0523@gmail.com>
2024-04-10 17:51:20 +0900

    refact : 리포지토리 수정

--- src/main/java/com/onmydesk/backend/page/PageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onmydesk.backend.page;
-
-import com.onmydesk.backend.page.domain.Page;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PageRepository extends JpaRepository<Page, Long> {
-}
--- src/main/java/com/onmydesk/backend/product/repository/ProductRepository.java
+++ src/main/java/com/onmydesk/backend/product/repository/ProductRepository.java
@@ -1,7 +1,13 @@
-package com.onmydesk.backend.product;
+package com.onmydesk.backend.product.repository;
 
 import com.onmydesk.backend.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByProductCode(String productCode);
+
+
 }
