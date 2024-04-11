582ae136073024dd851d49fbab7074918643071f
Jaehoon <gkdwngo@gmail.com>
2024-04-11 15:20:10 +0900

    refact : 전역 예외 처리 코드 수정

--- src/main/java/com/onmydesk/backend/global/ApiResponse.java
+++ src/main/java/com/onmydesk/backend/global/ApiResponse.java
@@ -14,20 +14,13 @@ public class ApiResponse {
     private static final String STATUS_FAIL = "fail";
     private static final String STATUS_ERROR = "error";
 
-    private  <T, E> ResponseEntity<?> get(String status, @Nullable String message, @Nullable T data, @Nullable E errors, HttpStatus httpStatus) {
+    private  <T, E> ResponseEntity<Object> get(String status, @Nullable String message, @Nullable T data, @Nullable E errors, HttpStatus httpStatus) {
 
         if (status.equals(STATUS_SUCCESS)) {
             return new ResponseEntity<>(SucceededBody.builder()
                     .status(status)
                     .message(message)
                     .data(data)
-                    .build(),
-                    httpStatus);
-        } else if (status.equals(STATUS_FAIL)) {
-            return new ResponseEntity<>(FailedBody.builder()
-                    .status(status)
-                    .message(message)
-                    .errors(errors)
                     .build(),
                     httpStatus);
         } else if (status.equals(STATUS_ERROR)) {
@@ -48,7 +41,7 @@ public class ApiResponse {
 //          "message" : "success message",
 //          "data" : "배열 또는 단일 데이터"
 //     }
-    public <T> ResponseEntity<?> success(String message, T data, HttpStatus httpStatus) {
+    public <T> ResponseEntity<Object> success(String message, T data, HttpStatus httpStatus) {
         return get(STATUS_SUCCESS, message, data, null, httpStatus);
     }
 
@@ -59,7 +52,7 @@ public class ApiResponse {
 //          "message" : null,
 //          "data" : "배열 또는 단일 데이터"
 //     }
-    public <T> ResponseEntity<?> success(T data, HttpStatus httpStatus) {
+    public <T> ResponseEntity<Object> success(T data, HttpStatus httpStatus) {
         return get(STATUS_SUCCESS, null, data, null, httpStatus);
     }
 
@@ -70,7 +63,7 @@ public class ApiResponse {
 //          "data" : null
 //     }
 
-    public <T> ResponseEntity<?> success(String message, HttpStatus httpStatus) {
+    public <T> ResponseEntity<Object> success(String message, HttpStatus httpStatus) {
         return get(STATUS_SUCCESS, message, null, null, httpStatus);
     }
 
@@ -80,39 +73,8 @@ public class ApiResponse {
 //          "message" : null,
 //          "data" : null
 //     }
-    public ResponseEntity<?> success(HttpStatus httpStatus) {
+    public ResponseEntity<Object> success(HttpStatus httpStatus) {
         return get(STATUS_SUCCESS, null, null, null, httpStatus);
-    }
-
-
-//     실패 응답 반환 (상태, 메시지, 에러)
-//     {
-//          "status" : "fail",
-//          "message" : fail message,
-//          "errors" : [{error data1}, {error data2} ... ]
-//     }
-    public <E> ResponseEntity<?> fail(String message, E errors, HttpStatus httpStatus) {
-        return get(STATUS_FAIL, message, null, errors, httpStatus);
-    }
-
-//     실패 응답 반환 (상태, 메시지)
-//     {
-//          "status" : "fail",
-//          "message" : "fail message",
-//          "errors" : null
-//     }
-    public ResponseEntity<?> fail(String message, HttpStatus httpStatus) {
-        return get(STATUS_FAIL, message, null, null, httpStatus);
-    }
-
-//     실패 응답 반환 (상태, 에러)
-//     {
-//          "status" : "fail",
-//          "message" : null,
-//          "errors" : [{error data1}, {error data2} ... ]
-//     }
-    public <E> ResponseEntity<?> fail(E errors, HttpStatus httpStatus) {
-        return get(STATUS_FAIL, null, null, errors, httpStatus);
     }
 
 //     예외 발생 시 에러 응답 반환
@@ -120,7 +82,7 @@ public class ApiResponse {
 //          "status" : "error",
 //          "message" : "custom error message"
 //     }
-    public ResponseEntity<?> error(String message, HttpStatus httpStatus) {
+    public ResponseEntity<Object> error(String message, HttpStatus httpStatus) {
         return get(STATUS_ERROR, message, null, null, httpStatus);
     }
 
@@ -135,19 +97,6 @@ public class ApiResponse {
         private String status;
         private String message;
         private T data;
-    }
-
-     // 실패 응답 객체의 바디
-    @Builder
-    @Setter
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class FailedBody<E> {
-
-        private String status;
-        private String message;
-        private E errors;
     }
 
     // 오류 응답 객체 바디
