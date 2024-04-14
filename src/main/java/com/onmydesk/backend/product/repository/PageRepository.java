5eb3ec1b7542ff50c4ec046ac3872f294ee80365
suha <suhach0523@gmail.com>
2024-04-15 01:10:53 +0900

    refact : 상품 리포지토리 리팩토링

--- src/main/java/com/onmydesk/backend/product/repository/PageRepository.java
+++ src/main/java/com/onmydesk/backend/product/repository/PageRepository.java
@@ -2,9 +2,11 @@ package com.onmydesk.backend.product.repository;
 
 import com.onmydesk.backend.product.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PageRepository extends JpaRepository<Page, Long> {
 
     List<Page> findByProductId(Long productId);
--- src/main/java/com/onmydesk/backend/product/repository/ProductRepository.java
+++ src/main/java/com/onmydesk/backend/product/repository/ProductRepository.java
@@ -1,13 +1,17 @@
 package com.onmydesk.backend.product.repository;
 
 import com.onmydesk.backend.product.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findByProductCode(String productCode);
 
-
+    Page<Product> findAll(Pageable pageable);
 }
