43e4903dc3e5f8c577a932e018a8f39c78e796b5
suha <suhach0523@gmail.com>
2024-05-04 12:08:47 +0900

    feat : Product 엔티티에 wish 속성 추가

--- src/main/java/com/onmydesk/backend/product/domain/Product.java
+++ src/main/java/com/onmydesk/backend/product/domain/Product.java
@@ -1,6 +1,7 @@
 package com.onmydesk.backend.product.domain;
 
 import com.onmydesk.backend.global.BaseEntity;
+import com.onmydesk.backend.wish.domain.Wish;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
@@ -64,4 +65,7 @@ public class Product extends BaseEntity {
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.REMOVE)
     private List<Page> pages = new ArrayList<>();
+
+    @OneToMany(mappedBy = "product")
+    private List<Wish> wish;
 }
