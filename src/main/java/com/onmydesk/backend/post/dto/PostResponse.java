e70c1dd318d7985090617b80c0d21a1bbb199fd7
jihye9871 <jihye9871@naver.com>
2024-05-09 03:28:01 +0900

    feat: post관련 dto에 이미지 관련 요청, 응답값 수정

---
+++ src/main/java/com/onmydesk/backend/post/dto/PostResponse.java
@@ -0,0 +1,33 @@
+package com.onmydesk.backend.post.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Singular;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class PostResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String nickname;
+    private final int heartCount;
+    private final int viewCount;
+    private final int totalPrice;
+    private final boolean isLiked;
+    @Singular("image")
+    private final List<String> imageUrls; // 게시글에 포함된 이미지 URL 리스트
+
+    private final String thumbnailUrl; // 썸네일 이미지의 URL
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    private final LocalDateTime createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    private final LocalDateTime updatedAt;
+
+}
