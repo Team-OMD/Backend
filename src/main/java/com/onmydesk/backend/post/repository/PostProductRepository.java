790e5ac1724d67ca6cbb26db4c34cc4eff1dcaf3
Jaehoon <gkdwngo@gmail.com>
2024-05-20 13:19:03 +0900

    refact : 게시글 상품 목록 조회 fetch join 적용

--- src/main/java/com/onmydesk/backend/post/repository/PostProductRepository.java
+++ src/main/java/com/onmydesk/backend/post/repository/PostProductRepository.java
@@ -2,6 +2,7 @@ package com.onmydesk.backend.post.repository;
 
 import com.onmydesk.backend.post.domain.PostProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.List;
 @Repository
 public interface PostProductRepository extends JpaRepository<PostProduct, Long> {
 
+    @Query("select pp from PostProduct pp " +
+            "join fetch pp.product " +
+            "where pp.post.id = :postId")
     List<PostProduct> findByPostId(Long postId);
 
     // 제품 ID에 대해 연결된 PostProduct의 수를 반환
