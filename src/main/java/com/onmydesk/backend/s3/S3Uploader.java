f8baa6d6a6665d70eb81eb86558ee1d1b561db66
jihye9871 <jihye9871@naver.com>
2024-06-03 22:38:38 +0900

    feat: 리사이징 로직 추가

--- src/main/java/com/onmydesk/backend/s3/S3Uploader.java
+++ src/main/java/com/onmydesk/backend/s3/S3Uploader.java
@@ -5,14 +5,17 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Optional;
+import javax.imageio.ImageIO;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,27 +26,30 @@ public class S3Uploader {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    // MultipartFile을 전달받아 File로 전환한 후 S3에 업로드
-    public String upload(MultipartFile multipartFile, String dirName) throws IOException { // dirName의 디렉토리가 S3 Bucket 내부에 생성됨
+    // MultipartFile을 전달받아 리사이즈된 이미지 파일로 전환한 후 S3에 업로드
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
 
         File uploadFile = convert(multipartFile)
                 .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
-        return upload(uploadFile, dirName);
+        File resizedFile = resizeImage(uploadFile); // 이미지 리사이징
+        String uploadImageUrl = upload(resizedFile, dirName); // 리사이즈된 파일 업로드
+
+        removeNewFile(uploadFile);  // 원본 파일 삭제
+        removeNewFile(resizedFile); // 리사이즈 파일 삭제
+
+        return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
     }
 
     private String upload(File uploadFile, String dirName) {
         String fileName = dirName + "/" + uploadFile.getName();
         String uploadImageUrl = putS3(uploadFile, fileName);
-
-        removeNewFile(uploadFile);  // convert()함수로 인해서 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
-
-        return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
+        return uploadImageUrl;
     }
 
     private String putS3(File uploadFile, String fileName) {
         amazonS3Client.putObject(
                 new PutObjectRequest(bucket, fileName, uploadFile)
-                        .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한으로 업로드 됨
+                        .withCannedAcl(CannedAccessControlList.PublicRead) // PublicRead 권한으로 업로드 됨
         );
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
@@ -66,4 +72,15 @@ public class S3Uploader {
         }
         return Optional.empty();
     }
+
+    // 이미지를 리사이징하는 메소드
+    private File resizeImage(File imageFile) throws IOException {
+        BufferedImage resizedImage = Thumbnails.of(imageFile) // Thumbnailator 라이브러리 사용
+                .size(512, 512)
+                .asBufferedImage();
+        File tempFile = new File(imageFile.getParent(), "resized_" + System.currentTimeMillis() + ".png");
+        ImageIO.write(resizedImage, "png", tempFile);
+        return tempFile;
+    }
+
 }
